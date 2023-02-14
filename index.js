a8620ee257c18aaf4b73ee144ee2742916e7a0ca
Rene Weteling <rene.weteling@t-mobile.nl>
2023-02-14 09:11:02 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    "@commitlint/config-conventional",
+  ],
+  plugins: ["commitlint-plugin-function-rules"],
+  rules: {
+    "function-rules/type-enum": [
+      2,
+      "always",
+      (parsed) => {
+        const headerRegex = /^(\w*)(\(([a-z]{2,8}-[0-9]+)\))?:(.+)$/g;
+        return parsed.header.match(headerRegex)
+          ? [true]
+          : [
+              false,
+              `Type must either have a jira ticket or be empty e.g: "chore: something" or chore(red-123): something`,
+            ];
+      },
+    ],
+  },
+};
